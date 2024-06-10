897af94d60cf01735f405507c7b74294d8924f81
wai4y <devdaniel.deng@gmail.com>
2024-06-10 19:57:14 +0900

    chore: update comment

--- src/main/java/com/test/order/service/OrderService.java
+++ src/main/java/com/test/order/service/OrderService.java
@@ -10,9 +10,27 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 
 public interface OrderService {
+    /**
+     * Place order with origin and destination coordinates
+     * @param orderRequest the request info
+     * @return created order info
+     */
     OrderDto placeOrder(OrderRequest orderRequest);
 
+    /**
+     * Take order with order id, update order status to SUCCESS
+     * @param id order id
+     * @param status order status to be updated, only accept TAKEN
+     * @return the updated order Success status
+     */
     TakeOrderDto takeOrder(Long id, OrderStatus status);
 
+
+    /**
+     * Get order list with pagination
+     * @param page page number, start from 1
+     * @param limit page size
+     * @return order list
+     */
     List<OrderDto> getOrderList(Integer page, Integer limit);
 }
